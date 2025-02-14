e8861113e9f8c30c13730005d40479004aaaa150
Fadi Bibawy <eng.fadimichel@gmail.com>
2025-02-14 01:30:15 +0100

    logic created

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,15 @@
 class Property < ApplicationRecord
+  def self.within_radius(lat, lng, radius, property_type, marketing_type)
+    query = <<-SQL.squish
+      SELECT *,
+      earth_distance(ll_to_earth(?, ?), ll_to_earth(lat::double precision, lng::double precision)) AS distance
+      FROM properties
+      WHERE property_type = ?
+        AND offer_type = ?
+        AND earth_distance(ll_to_earth(?, ?), ll_to_earth(lat::double precision, lng::double precision)) <= ?
+      ORDER BY distance ASC
+    SQL
+
+    find_by_sql([query, lat, lng, property_type, marketing_type, lat, lng, radius])
+  end
 end
