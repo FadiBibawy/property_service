1425cf1455df146d733a8f8ff32760273f620f91
Fadi Bibawy <eng.fadimichel@gmail.com>
2025-02-14 20:22:11 +0100

    remove test folder

--- test/controllers/properties_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class PropertiesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get properties_index_url
-    assert_response :success
-  end
-end
